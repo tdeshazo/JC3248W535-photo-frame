118076bb8fc5a387b8cd3ceb6ba492677b406a8a
GthiN <Petr.belohoubek89@gmail.com>
2024-10-30 07:16:33 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+JC3248W535EN - Cheap black display
+<br>
+based on ESP32_Display_Panel LVGL example and LVGL_DEMO from maufactuer<br>
+Do compile<br>
+Is Platformio<br>
+Is Arduino<br>
+Acctualy work!!!!!!!!!!!!!!!!!!!!<br>
+Im begginer, co took me like 3 days :D<br>
---
+++ src/app.cpp
@@ -0,0 +1,80 @@
+
+// #include <Arduino.h>
+#include <lvgl.h>
+#include "display.h"
+#include "esp_bsp.h"
+#include "lv_port.h"
+
+
+
+/**
+ * @brief LVGL porting example
+ * Set the rotation degree:
+ *      - 0: 0 degree
+ *      - 90: 90 degree
+ *      - 180: 180 degree
+ *      - 270: 270 degree
+ *
+ */
+#define LVGL_PORT_ROTATION_DEGREE (90)
+
+
+void setup();
+
+
+void setup()
+{
+
+
+
+  size_t freePsram = heap_caps_get_free_size(MALLOC_CAP_SPIRAM);
+  bsp_display_cfg_t cfg = {
+      .lvgl_port_cfg = ESP_LVGL_PORT_INIT_CONFIG(),
+      .buffer_size = EXAMPLE_LCD_QSPI_H_RES * EXAMPLE_LCD_QSPI_V_RES,
+#if LVGL_PORT_ROTATION_DEGREE == 90
+      .rotate = LV_DISP_ROT_90,
+#elif LVGL_PORT_ROTATION_DEGREE == 270
+      .rotate = LV_DISP_ROT_270,
+#elif LVGL_PORT_ROTATION_DEGREE == 180
+      .rotate = LV_DISP_ROT_180,
+#elif LVGL_PORT_ROTATION_DEGREE == 0
+      .rotate = LV_DISP_ROT_NONE,
+#endif
+  };
+
+  bsp_display_start_with_config(&cfg);
+  bsp_display_backlight_on();
+
+  /* Lock the mutex due to the LVGL APIs are not thread-safe */
+  bsp_display_lock(0);
+
+
+   
+    lv_obj_t * label;
+
+    lv_obj_t * btn1 = lv_btn_create(lv_scr_act());
+   // lv_obj_add_event_cb(btn1, event_handler, LV_EVENT_ALL, NULL);
+    lv_obj_align(btn1, LV_ALIGN_CENTER, 0, -40);
+
+    label = lv_label_create(btn1);
+    lv_label_set_text(label, "Button");
+    lv_obj_center(label);
+
+    lv_obj_t * btn2 = lv_btn_create(lv_scr_act());
+   // lv_obj_add_event_cb(btn2, event_handler, LV_EVENT_ALL, NULL);
+    lv_obj_align(btn2, LV_ALIGN_CENTER, 0, 40);
+    lv_obj_add_flag(btn2, LV_OBJ_FLAG_CHECKABLE);
+    lv_obj_set_height(btn2, LV_SIZE_CONTENT);
+
+    label = lv_label_create(btn2);
+    lv_label_set_text(label, "Toggle");
+    lv_obj_center(label);
+
+  bsp_display_unlock();
+
+}
+
+void loop()
+{
+
+}
